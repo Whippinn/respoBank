eae9b1fb0cfe6965448a9f17de6aae88477e6d79
respect <79569914+respect0@users.noreply.github.com>
2021-08-18 13:39:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) throws SQLException, InterruptedException {
         user user = new user();
-        //user.registerAcc();
+        user.registerAcc();
         user.loginAcc();
     }
 }
